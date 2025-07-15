da75f1565e6f21ec097e4595813c8bfcd3991b49
V3RM1Ns <rovsanoglu@gmail.com>
2025-07-15 12:06:15 +0400

    asd

---
+++ Orm.DAL/Migrations/20250711162523_Table.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Orm.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class Table : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderItems_MenuItems_MenuItemId",
+                table: "OrderItems");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderItems_Orders_OrderId",
+                table: "OrderItems");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Tables_TableId",
+                table: "Orders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_OrderItems_MenuItemId",
+                table: "OrderItems");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TableId",
+                table: "Orders",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "OrderDate",
+                table: "Orders",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValueSql: "GETDATE()");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "TotalOrderPrice",
+                table: "Orders",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "TotalAmount",
+                table: "OrderItems",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<int>(
+                name: "MenuItemId1",
+                table: "OrderItems",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderItems_MenuItemId",
+                table: "OrderItems",
+                column: "MenuItemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderItems_MenuItemId1",
+                table: "OrderItems",
+                column: "MenuItemId1",
+                unique: true,
+                filter: "[MenuItemId1] IS NOT NULL");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderItems_MenuItems_MenuItemId",
+                table: "OrderItems",
+                column: "MenuItemId",
+                principalTable: "MenuItems",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderItems_MenuItems_MenuItemId1",
+                table: "OrderItems",
+                column: "MenuItemId1",
+                principalTable: "MenuItems",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderItems_Orders_OrderId",
+                table: "OrderItems",
+                column: "OrderId",
+                principalTable: "Orders",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Tables_TableId",
+                table: "Orders",
+                column: "TableId",
+                principalTable: "Tables",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderItems_MenuItems_MenuItemId",
+                table: "OrderItems");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderItems_MenuItems_MenuItemId1",
+                table: "OrderItems");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderItems_Orders_OrderId",
+                table: "OrderItems");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Tables_TableId",
+                table: "Orders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_OrderItems_MenuItemId",
+                table: "OrderItems");
+
+            migrationBuilder.DropIndex(
+                name: "IX_OrderItems_MenuItemId1",
+                table: "OrderItems");
+
+            migrationBuilder.DropColumn(
+                name: "TotalOrderPrice",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "MenuItemId1",
+                table: "OrderItems");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TableId",
+                table: "Orders",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "OrderDate",
+                table: "Orders",
+                type: "datetime2",
+                nullable: false,
+                defaultValueSql: "GETDATE()",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TotalAmount",
+                table: "OrderItems",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderItems_MenuItemId",
+                table: "OrderItems",
+                column: "MenuItemId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderItems_MenuItems_MenuItemId",
+                table: "OrderItems",
+                column: "MenuItemId",
+                principalTable: "MenuItems",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderItems_Orders_OrderId",
+                table: "OrderItems",
+                column: "OrderId",
+                principalTable: "Orders",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Tables_TableId",
+                table: "Orders",
+                column: "TableId",
+                principalTable: "Tables",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
